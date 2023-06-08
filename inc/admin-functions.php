dfcc8651afcc1b1c68fba1c2810a63fe58343ca3
osdal <osdal67@gmail.com>
2023-06-08 22:42:46 +0300

    Добавил комментарии

--- inc/admin-functions.php
+++ inc/admin-functions.php
@@ -5,32 +5,51 @@ function add_admin_page_setting_page(){
 	$hook_suffix = add_menu_page( 'Страница настроек', 
 											'Мои настройки', 
 											'edit_others_posts', 
-											'page-options', 'create_settings_options_page',
+											'page-options', 
+											'create_settings_options_page',
 											'');
-						add_submenu_page(	'page-options',
-							'Настройки главной страницы', 
-							'Настройки главной', 
-							'edit_others_posts', 
-							'frontpage-options', 
-							'create_frontpage_options', 
-							'dashicons-admin-generic' );
+						add_submenu_page(	
+							'page-options',//Название (slug) родительского меню в которое будет добавлен пункт или название файла админ-страницы WordPress.
+							'Настройки главной страницы',//Текст, который будет использован в теге title на странице.
+							'Настройки главной', //Текст, который будет использован как название пункта меню
+							'edit_others_posts', //Возможность пользователя, чтобы иметь доступ к меню.
+							'frontpage-options', //Уникальное название (slug), по которому затем можно обращаться к этому меню.
+							'create_frontpage_options', //Название функции которая будет вызваться, чтобы вывести контент создаваемой страницы
+							'dashicons-admin-generic' );//Иконка
 
-						}
+}
+
 add_action('admin_init', 'custom_settings');
 
 function custom_settings(){
-	register_setting( 'frontpage_group', 'title_frontpage' );
+	//Регистрирует новую опцию и callback функцию для обработки значения опции при её сохранении в БД.
+	register_setting(  'frontpage_group',/*Название группы, к которой будет принадлежать опция. 
+														Это название должно совпадать с названием группы в функции settings_fields().*/
+	 						  'title_frontpage' );//Название опции, которая будет сохраняться в БД.
 
-	add_settings_section( 'frontpage_group_section', 'Текстовые настройки', '', 'frontpage-options');
+	add_settings_section('frontpage_group_section',/*Идентификатор секции, по которому нужно "цеплять" поля к секции. 
+																	Строка, которая будет использована для id атрибутов тегов.*/
+								'Текстовые настройки',//Заголовок секции.
+								'',//Коллбэк (функция), которая заполняет секцию описанием. Вызывается перед выводом полей.
+								'frontpage-options');//Страница на которой выводить секцию.
 
-	add_settings_field( 'field_title_frontpage', 
-								'Заголовок главной страницы', 
-								'frontpage_title_field', 
-								'frontpage-options', 
-								'frontpage_group_section',
-								array('label_for' => 'field_title_frontpage'));
+	
+	//Создает поле опции для указанной секции (указанного блока настроек).
+	add_settings_field( 'field_title_frontpage', /*Ярлык (slug) опции, используется как идентификатор поля. 
+																Используется в ID атрибуте тега.*/
+								'Заголовок главной страницы', //Название поля.
+								'frontpage_title_field', /*Название функции обратного вызова. Функция должна заполнять поле нужным <input> тегом, который станет частью одной большой формы.
+																	Атрибут nameдолжен быть равен параметру $option_name из register_setting().
+																	Атрибут idобычно равен параметру $id.
+																	Результат должен сразу выводиться на экран (echo).*/
+								'frontpage-options',//Страница меню в которую будет добавлено поле.  
+								'frontpage_group_section',//Название секции настроек, в которую будет добавлено поле. 
+								array('label_for' => 'field_title_frontpage'));/*Дополнительные параметры, 
+																								которые нужно передать callback функции. 
+																								label_for - Строка. Если указать, то заголовок настройки будет обернут тегом 
+																								<label for="ЗНАЧЕНИЕ ПОЛЯ">*/
 }
-
+						
 function frontpage_title_field(){
 	$content_title_fronpage = esc_attr(get_option('title_frontpage')) ;
 
--- inc/templates/frontpage_settings.php
+++ inc/templates/frontpage_settings.php
@@ -1,11 +1,10 @@
 <h1>Настройки главной страницы</h1>
 
+<?php settings_errors(); ?>
+
 <form action="options.php" method="post">
 	<?php settings_fields( 'frontpage_group' ); ?>
 	<?php do_settings_sections( 'frontpage-options' ); ?>
 
 	<?php submit_button() ?>
-
-
-
 </form>
